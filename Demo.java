a2161a52dfd2f3b5d21ba83ff4b9d7972fca6bce
meryl-25 <149992491+meryl-25@users.noreply.github.com>
2024-02-27 15:52:36 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Demo
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter your Name : ");
+
+		String name = sc.nextLine();
+
+		System.out.println(" Username is : " +name);
+	}
+
+}
