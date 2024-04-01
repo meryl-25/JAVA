1fe62e420c9ad96594b422a31d67367b7f9fd822
meryl-25 <149992491+meryl-25@users.noreply.github.com>
2024-04-01 15:52:31 +0530

    Add files via upload

---
+++ Q5Program.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Q5Program
+{
+    int price;
+
+        class processor
+	{
+        	int cores;
+        	String producer;
+        	processor(int noC, String manu)
+		{
+           	 	cores=noC;
+            		producer=manu;
+        	}
+
+       		 void display()
+		{
+       			System.out.println("\nProcessor info");
+        		System.out.println("No. of Cores = "+cores);
+        		System.out.println("Manufacturer = "+producer+"\n");
+    		}
+	}
+
+	
+	static class ram
+	{
+       		int mem;
+        	String manuf;
+        	ram(int memory,String producer )
+		{
+            		mem=memory;
+            		manuf=producer;
+        	}
+
+        	void display()
+		{
+        		System.out.println("\nRAM info");
+        		System.out.println("Memory = "+mem+" GB");
+        		System.out.println("Manufacturer = "+manuf+"\n");
+    		}
+	}
+   
+	public static void main(String[] args) 
+	{
+         	Q5Program.ram obj1= new Q5Program.ram(8,"Intel");
+         	Q5Program obj2 = new Q5Program();
+         	Q5Program.processor obj3 = obj2.new processor(8,"Samsung");
+         	obj1.display();
+         	obj3.display();
+
+    }
+}
