1f8a3aaf0c4ce06e5c17f33c697b8bb3318ca653
meryl-25 <149992491+meryl-25@users.noreply.github.com>
2024-04-18 12:18:01 +0530

    Add files via upload

---
+++ Q10Program.java
@@ -0,0 +1,4 @@
+/*
+Create a class Employee having attributes eNo, eName eSalary. Read 'N' employ information and Search for an employee given eNo, using
+the concept of Array of Objects.
+*/
---
+++ Q8Program.java
@@ -0,0 +1,1 @@
+//Perform string manipulations 
